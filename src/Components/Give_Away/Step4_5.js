7d4a8a2051dec64f406355f4b202133d9f1287bf
Paweł Kubiak <ffreakpl@MacBook-Air-Pawe.local>
2022-07-12 01:53:02 +0200

    testing context

--- src/Components/Give_Away/Step4_5.js
+++ src/Components/Give_Away/Step4_5.js
@@ -19,20 +19,20 @@ export default function Step4_5() {
     }
 
     return (
-        <div className="step"><div style={{display: 'flex'}}>
-            <div style={{display: 'flex', flexDirection: `column`}}>
-                <h1 style={{fontSize: "4rem", marginBottom: "2rem", marginTop: "4rem"}}>Test state'ów:</h1>
-                <p style={{fontSize: "2rem", marginBottom: "2rem"}}><span>Rzeczy do oddania: </span>
+        <div className="step"><div style={{display: 'flex', height: "100%"}}>
+            <div style={{display: 'flex', flexDirection: `column`, height: "90px"}}>
+                <h1 style={{fontSize: "3rem", marginBottom: "1rem", marginTop: "1rem"}}>Test state'ów:</h1>
+                <p style={{fontSize: "2rem", marginBottom: "1rem"}}><span>Rzeczy do oddania: </span>
                     <span style={{fontWeight: "700"}}>{state.selectedThing}</span></p>
-                <p style={{fontSize: "2rem", marginBottom: "2rem"}}><span>Ilość toreb: </span>
+                <p style={{fontSize: "2rem", marginBottom: "1rem"}}><span>Ilość toreb: </span>
                     <span style={{fontWeight: "700"}}>{state.selectedBag}</span></p>
-                <p style={{fontSize: "2rem", marginBottom: "2rem"}}><span>Wybrana lokalizacja: </span>
+                <p style={{fontSize: "2rem", marginBottom: "1rem"}}><span>Wybrana lokalizacja: </span>
                     <span style={{fontWeight: "700"}}>{state.selectedLocation}</span></p>
-                <p style={{fontSize: "2rem", marginBottom: "2rem"}}><span>Komu chcesz pomóc: </span>
+                <p style={{fontSize: "2rem", marginBottom: "1rem"}}><span>Komu chcesz pomóc: </span>
                     <span style={{fontWeight: "700"}}>{state.selectedHelp.join(",")}</span></p>
-                <p style={{fontSize: "2rem", marginBottom: "2rem"}}><span>Konkretna organizacja: </span>
+                <p style={{fontSize: "2rem", marginBottom: "1rem"}}><span>Konkretna organizacja: </span>
                     <span style={{fontWeight: "700"}}>{state.specificLocation}</span></p>
-                <p style={{fontSize: "2rem", marginBottom: "2rem"}}><span>Dane z formularza:</span></p>
+                <p style={{fontSize: "2rem", marginBottom: "1rem"}}><span>Dane z formularza:</span></p>
                 <span style={{fontWeight: "700",marginBottom: "1rem"}}>Ulica: {state.contact.street}</span>
                 <span style={{fontWeight: "700",marginBottom: "1rem"}}>Miasto: {state.contact.city}</span>
                 <span style={{fontWeight: "700",marginBottom: "1rem"}}>Kod pocztowy: {state.contact.postalCode}</span>
