eeead51685dedfcea3016902412ce64d2c43c971
Paweł Kubiak <ffreakpl@MacBook-Air-Pawe.local>
2022-07-12 09:27:21 +0200

    small change

--- src/Components/Home_Footer.js
+++ src/Components/Home_Footer.js
@@ -7,7 +7,7 @@ export default function Home_Footer() {
     return (
         <div className="footer">
             <div className="footer_container">
-                <div className="footer_item">Copyright by Coders Lab</div>
+                <div className="footer_item">Coded by FFREAK.</div>
                 <div className="footer_logos">
                     <div className="footer_logo"><img src={Facebook} alt="facebook"/></div>
                     <div className="footer_logo"><img src={Instagram} alt="instagram"/></div>
