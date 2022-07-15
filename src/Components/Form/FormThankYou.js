3877645f14ad7751eca427c224aca6543bd2ba2c
Paweł Kubiak <ffreakpl@MacBook-Air-Pawe.local>
2022-07-15 16:21:15 +0200

    dodano Thank You

--- src/Components/Form/FormThankYou.js
+++ src/Components/Form/FormThankYou.js
@@ -1,12 +1,18 @@
 import React, {useContext, useState} from "react";
-import FormButton from "../Buttons/ButtonForm";
+import Decoration from "../Decorations/Decoration";
 import {DataContext} from "./FormStepsHome";
 
 export default function FormThankYou(){
     const {state, setState} = useContext(DataContext);
 
     return (
-        <>
-        </>
+        <div className="step">
+            <div className="step_thankYou">
+                <p>Dziękujemy za przesłanie formularza</p>
+                <p>Na maila prześlemy wszelkie</p>
+                <p>informacje o odbiorze.</p>
+                <Decoration />
+            </div>
+        </div>
     )
 }
